df3d93e12e40105cdb896fa8774e62ba94054923
jerrypeng0112 <jerrypeng0112@hotmail.com>
2018-12-18 21:47:01 -0800

    Fixed some code in Day3 and template

---
+++ Day 4/q1.py
@@ -0,0 +1,18 @@
+def main():
+    data = readFiles()
+
+    print(result)
+
+def processFileLine(line):
+    
+    return int(line)
+
+def readFiles():
+    data = []
+    file = open("input.txt", "r")
+    for line in file:
+        data.append(processFileLine(line))
+    return data
+
+if __name__ == "__main__":
+    main()
