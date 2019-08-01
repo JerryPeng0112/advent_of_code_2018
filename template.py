34e4e89a3cc37e640f21916e6e3a515a269abe92
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-07-31 18:06:00 -0700

    Added statement to close input file

--- template.py
+++ template.py
@@ -4,15 +4,21 @@ def main():
 
     print(input)
 
+
 def readLine(line):
     return line
+
 
 def readFiles():
     data = []
     file = open("input.txt", "r")
+
     for line in file:
         data.append(readLine(line))
+
+    file.close()
     return data
+
 
 if __name__ == "__main__":
     main()
