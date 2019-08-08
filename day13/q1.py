50e5fcd7438b2aff9b28fab2c9e3096b49044b50
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-08-07 22:19:32 -0700

    Setup day 13

---
+++ day13/q1.py
@@ -0,0 +1,24 @@
+def main():
+
+    data = readFiles()
+
+    print(data)
+
+
+def readLine(line):
+    return line
+
+
+def readFiles():
+    data = []
+    file = open("input.txt", "r")
+
+    for line in file:
+        data.append(readLine(line))
+
+    file.close()
+    return data
+
+
+if __name__ == "__main__":
+    main()
