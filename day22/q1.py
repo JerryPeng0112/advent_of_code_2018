7e68c1c44d64902a69634d2b9a106ac54331e8cd
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-09-13 12:55:23 -0700

    FInished day22 q1

--- day22/q1.py
+++ day22/q1.py
@@ -1,23 +1,74 @@
+import re
+
+
 def main():
 
-    data = readFiles()
+    fileStr = readFiles()
 
-    print(data)
+    depth, target = getData(fileStr)
+
+    eroLevelTable = buildEroLevelTable(depth, target)
+    
+    riskLevel = calcRiskLevel(eroLevelTable)
+
+    print(riskLevel)
 
 
-def readLine(line):
-    return line
+def calcRiskLevel(eroLevelTable):
+    return sum(map(sum, eroLevelTable))
+
+
+def buildEroLevelTable(depth, target):
+    targetX = target[0]
+    targetY = target[1]
+    mod = 20183
+
+    table = [[0 for i in range(targetX + 1)] for i in range(targetY + 1)]
+
+    # Calculate the table after applying depth and erosion level modulo
+    # Optimized so that actual large number multiplications do not take place
+    for i in range(targetY + 1):
+        for j in range(targetX + 1):
+                
+            indexVal = 0
+            if (i == 0 and j == 0) or (i == targetY and j == targetX):
+                indexVal = depth % mod
+            elif i == 0:
+                indexVal = (j * 16807 + depth) % mod
+            elif j == 0:
+                indexVal = (i * 48271 + depth) % mod
+            else:
+                indexVal = (table[i - 1][j] * table[i][j - 1] + depth) % mod
+
+            table[i][j] = indexVal
+
+    # Get erosion level by modulo 3 to all numbers
+    for i in range(targetY + 1):
+        for j in range(targetX + 1):
+            table[i][j] %= 3
+
+    return table
+
+
+
+
+def getData(fileStr):
+    # Get the depth and target integers
+    strList = fileStr.split('\n')
+    depth = int(re.findall(r'\d+', strList[0])[0])
+    target = re.findall(r'\d+', strList[1])
+    targetX = int(target[0])
+    targetY = int(target[1])
+    target = [targetX, targetY]
+
+    return depth, target
 
 
 def readFiles():
-    data = []
-    file = open('input.txt', 'r')
-
-    for line in file:
-        data.append(readLine(line))
-
-    file.close()
-    return data
+    f = open('input.txt', 'r')
+    fileStr = f.read()
+    f.close()
+    return fileStr
 
 
 if __name__ == '__main__':
